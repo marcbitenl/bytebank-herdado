a6d84a8dc6d2d8274e41fd08e0e60d3fc6d360fc
marcb <marcbitenl@gmail.com>
2022-04-08 01:30:39 -0300

    Introducao ao polimorfismo

---
+++ src/TesteReferencias.java
@@ -0,0 +1,16 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class TesteReferencias {
+
+	public static void main(String[] args) {
+		
+		Gerente g1 = new Gerente();
+		
+		g1.setNome("Marcos");
+		String nome = g1.getNome();
+		
+		System.out.println(nome);
+
+	}
+
+}
