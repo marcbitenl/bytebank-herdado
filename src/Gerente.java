e681603758c4d19543eecc536276f6e1c3cee556
marcb <marcbitenl@gmail.com>
2022-04-11 19:59:32 -0300

    Revendo a composicao

--- src/Gerente.java
+++ src/Gerente.java
@@ -2,10 +2,10 @@
 //Gerente herda da classe funcionario, assina o contrato autenticavél, e um um autenticavel 
 public class Gerente extends Funcionario implements Autenticavel {
 
-	private int senha;
+	private AutenticacaoUtil autenticador;
 
 	public Gerente() {
-
+		this.autenticador = new AutenticacaoUtil();
 	}
 
 	public double getBonificacao() {
@@ -15,17 +15,13 @@ public class Gerente extends Funcionario implements Autenticavel {
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
-		
+		this.autenticador.setSenha(senha);
+
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+		return this.autenticador.autentica(senha);
 
+	}
 }
